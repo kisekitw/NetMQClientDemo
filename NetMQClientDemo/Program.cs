e712ba21aef026abf10e03a5c3d4da1a3726263b
kisekitw <kisekitw@gmail.com>
2018-10-13 06:48:55 +0800

    test sending client message to server

--- NetMQClientDemo/Program.cs
+++ NetMQClientDemo/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NetMQ;
+using NetMQ.Sockets;
+using System;
 
 namespace NetMQClientDemo
 {
@@ -10,6 +8,19 @@ namespace NetMQClientDemo
     {
         static void Main(string[] args)
         {
+            using (var client = new RequestSocket())
+            {
+                client.Connect("tcp://localhost:5555");
+
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("Sending Hello");
+                    client.SendFrame("Hello");
+
+                    var message = client.ReceiveFrameString();
+                    Console.WriteLine("Received {0}", message);
+                }
+            }
         }
     }
 }
